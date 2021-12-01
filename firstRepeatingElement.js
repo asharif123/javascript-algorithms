86284db981cc76d2f57898e6791e13f755eaa2b8
Awad Sharif <awadsharif9@gmail.com>
2021-12-01 00:25:51 -0800

    updated firstRepeatingElement algorithm

--- firstRepeatingElement.js
+++ firstRepeatingElement.js
@@ -4,12 +4,27 @@
 // //using filter, iterate thru each character in array
 // //filter out characters if indices after current character we are analyzing exists
 // //return first index in filtered array
-const firstRepeatingLetter = (sentence) => {
-    const newArray = sentence.toLowerCase().replaceAll(' ', '').split('');
-    const filteredArray = newArray.filter((letter) => newArray.indexOf(letter) !== newArray.lastIndexOf(letter))
-    return filteredArray[0];     
+// const firstRepeatingElement = (sentence) => {
+//     const newArray = sentence.toLowerCase().replaceAll(' ', '').split('');
+//     const filteredArray = newArray.filter((letter) => newArray.indexOf(letter) !== newArray.lastIndexOf(letter))
+//     return filteredArray[0];     
+// }
+
+
+
+// use for loop to iterate through each letter in sentence
+// if index of letter we are currently at is NOT equal to last occurence of that letter, return that letter
+// time: O(n), space: O(1)
+
+const firstRepeatingElement = (sentence) => {
+    for (let i = 0; i < sentence.length; i++) {
+        if (i !== sentence.lastIndexOf(sentence[i])) {
+            return sentence[i];
+        }
+    }
 }
 
-console.log(firstRepeatingLetter("James Reiher")); //e
-console.log(firstRepeatingLetter("First Repeating Letter")); //i
-console.log(firstRepeatingLetter("Geeks for Geeks")); //g
+console.log(firstRepeatingElement("James Reiher")); //e
+console.log(firstRepeatingElement("First Repeating Letter")); //i
+console.log(firstRepeatingElement("Geeks for Geeks")); //g
+console.log(firstRepeatingElement("Geeks for eeks")); //g
