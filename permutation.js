86934c303782f9b86a6c39c7fb64e0340bdcb472
Awad Sharif <awadsharif9@gmail.com>
2021-12-24 12:26:02 -0800

    permutation algorithm

--- permutation.js
+++ permutation.js
@@ -40,5 +40,47 @@
 //   });
 // });
 
+//STEPS:
+//take each letter in sub and create object, if letter is in object increment its count, else assign to 1
+//loop through each letter in str and if letter in sub exists, decrement its count
+//at end of for loop, do another for loop on sub and see if each letter in sub is equal to count of 0 (0 occurences) in object
+//if the count of letters in object is greater than 0 for any letter in object, return false
+//else if count of every letter in object is 0, return true
 
-var permutationSubstring = function(str, sub) {};
+const permutationSubstring = (str, sub) => {
+    let subLetters = {};
+    for (let i = 0; i < sub.length; i++) {
+        if (subLetters[sub[i]]) {
+            subLetters[sub[i]]+=1
+        }
+        else {
+            subLetters[sub[i]]=1
+
+        }
+    }
+
+    
+
+    for (let i = 0; i < str.length; i++) {
+        if (subLetters[str[i]]) {
+            subLetters[str[i]]-=1
+        }
+    }
+    
+
+    for (let i = 0; i < sub.length; i++) {
+        if (subLetters[sub[i]] > 0) {
+            return false;
+        }
+    }
+
+    return true;
+
+    
+    
+};
+
+console.log(permutationSubstring("tbacowa", "bat"))
+console.log(permutationSubstring("nAtsuiPlwuan", "Austin"))
+console.log(permutationSubstring("nnhsuieaero", "summer"))
+console.log(permutationSubstring("octmantisplantic", "atlantic ocean"))
